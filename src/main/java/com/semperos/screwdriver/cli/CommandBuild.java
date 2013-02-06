ec09a5661ac016bb2de4490e714657088034e68c
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-02-06 17:48:40 -0500

    Stub sub-command for 'build'

--- src/main/java/com/semperos/screwdriver/cli/CommandBuild.java
+++ src/main/java/com/semperos/screwdriver/cli/CommandBuild.java
@@ -1,5 +1,8 @@
 package com.semperos.screwdriver.cli;
 
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
 /**
  * Created with IntelliJ IDEA.
  * User: semperos
@@ -7,5 +10,9 @@ package com.semperos.screwdriver.cli;
  * Time: 3:51 PM
  * To change this template use File | Settings | File Templates.
  */
+@Parameters(commandDescription = "Build all assets")
 public class CommandBuild {
+
+    @Parameter(names = { "-o", "--optimize" })
+    public boolean optimize = false;
 }
