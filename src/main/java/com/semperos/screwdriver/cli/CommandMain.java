afa04b4523cd79e7ff5d011b60edfed53b9d33d2
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-02-06 17:49:07 -0500

    Start of primary CLI options, stubs for debug mode, help, and setting paths

--- src/main/java/com/semperos/screwdriver/cli/CommandMain.java
+++ src/main/java/com/semperos/screwdriver/cli/CommandMain.java
@@ -1,5 +1,8 @@
 package com.semperos.screwdriver.cli;
 
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
 /**
  * Created with IntelliJ IDEA.
  * User: semperos
@@ -7,5 +10,15 @@ package com.semperos.screwdriver.cli;
  * Time: 4:12 PM
  * To change this template use File | Settings | File Templates.
  */
+@Parameters(commandDescription = "Run the Screwdriver build tool")
 public class CommandMain {
+    @Parameter(names = { "-d", "--debug" }, description = "Debug mode")
+    public boolean debug = false;
+
+    @Parameter(names = { "-h", "--help" }, help = true)
+    public boolean help;
+
+    @Parameter(names = { "-b", "--base-path" }, description = "The base path from which to calculate asset paths")
+    public String basePath = System.getProperty("user.dir");
+
 }
