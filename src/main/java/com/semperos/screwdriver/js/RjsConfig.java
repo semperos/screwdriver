baa8d8047665326207b77e6a0afc47dc520f8fd2
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-02-14 11:31:50 -0500

    Rename r.js related class

---
+++ src/main/java/com/semperos/screwdriver/js/RjsConfig.java
@@ -0,0 +1,11 @@
+package com.semperos.screwdriver.js;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: semperos
+ * Date: 2/14/13
+ * Time: 11:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RjsConfig {
+}
