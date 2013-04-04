2747aa2c950deb95a99ef937c23c8d7db17d5f86
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-04-04 15:39:30 -0400

    Make it clearer when the r.js optimizer starts

--- src/main/resources/com/semperos/screwdriver/js/extension/process-rjs.js
+++ src/main/resources/com/semperos/screwdriver/js/extension/process-rjs.js
@@ -1,5 +1,6 @@
 //Create a runner that will run a separate build for each item
 //in the configs array. Thanks to @jwhitley for this cleverness
+print("Starting RequireJS (r.js) Optimizer...");
 for (var i = 0; i < __Screwdriver.rjs.moduleConfigs.length; i++) {
     requirejs.optimize(__Screwdriver.rjs.moduleConfigs[i], function(x) {
         print("Screwdriver RequireJS Optimizer (r.js)");
