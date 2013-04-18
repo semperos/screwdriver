ccdcb47e4effaeb21488da11f5618c0df40286c8
rheol <korlov@bericotechnologies.com>
2013-04-18 16:00:14 -0400

    With his gracious permission, added files from @wokier's java-to-OS-notify project to be used for task tray notification

---
+++ src/main/java/notify/README.txt
@@ -0,0 +1,4 @@
+The code in this package is copied from Francois @wokier Wauquier's java-to-OS-notify tool at https://github.com/wokier/java-to-OS-notify with his personal permission (Apache 2.0 license).
+Where appropriate, header files explain any modifications done to them.
+
+Once Maven artifacts are available for the tool, it will be imported using pom.xml and these copies will be removed.
---
+++ src/main/java/notify/jre6/Jre6Notifier.java
@@ -0,0 +1,73 @@
+package notify.jre6;
+
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import notify.MessageType;
+import notify.Notifier;
+import notify.UnableToNotifyException;
+
+/**
+ * Java Implementation
+ * 
+ * @author francois wauquier
+ * 
+ */
+public class Jre6Notifier implements Notifier {
+
+    @Override
+    public boolean isSupported() {
+	try {
+	    Class.forName("java.awt.SystemTray");
+	    return SystemTray.isSupported();
+	} catch (ClassNotFoundException e) {
+	    return false;
+	}
+    }
+
+    @Override
+    public void notify(MessageType messageType, String title, String message) {
+	try {
+	    final SystemTray systemTray = SystemTray.getSystemTray();
+	    String imageName = messageType.name().toLowerCase() + ".png";
+	    Image image = Toolkit.getDefaultToolkit().createImage(readImage(imageName));
+	    final TrayIcon trayIcon = new TrayIcon(image, title);
+	    systemTray.add(trayIcon);
+	    trayIcon.displayMessage(title, message, TrayIcon.MessageType.valueOf(messageType.name()));
+	    new Thread(new Runnable() {
+		@Override
+		public void run() {
+		    try {
+			Thread.sleep(5000);
+		    } catch (InterruptedException e) {
+			// ignore
+		    }
+		    systemTray.remove(trayIcon);
+		}
+	    }).start();
+	} catch (Exception e) {
+	    throw new UnableToNotifyException("Unable to notify with java", e);
+	}
+    }
+
+    private byte[] readImage(String imageName) throws IOException {
+	InputStream imageStream = getClass().getClassLoader().getResourceAsStream(imageName);
+	if (imageStream == null) {
+	    throw new FileNotFoundException(imageName);
+	}
+	ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+	byte[] buf = new byte[0x1000];
+	int b = imageStream.read(buf);
+	while (b != -1) {
+	    bytes.write(buf, 0, b);
+	    b = imageStream.read(buf);
+	}
+	return bytes.toByteArray();
+    }
+}
